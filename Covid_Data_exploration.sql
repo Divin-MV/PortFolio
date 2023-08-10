9553297cc25b3fed261a77bf9d017913c2112dde
Divin-MV <96161127+Divin-MV@users.noreply.github.com>
2023-08-10 23:00:22 +0100

    Add files via upload
    covid sql query second commit

--- Covid_Data_exploration.sql
+++ Covid_Data_exploration.sql
@@ -3,6 +3,7 @@ FROM dbo.coviddeaths
 ORDER BY 3,4
 
 
+
 SELECT * 
 FROM dbo.covidvacc
 ORDER BY 3,4
@@ -137,7 +138,7 @@ ORDER BY 2,3
 ---Creating View to store data for visualization
 
 
-CREATE VIEW PercentPopVaccinated as
+CREATE VIEW PercentPopVaccinated AS
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 SUM(CONVERT(float,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location,dea.date) as CumulativeVac
 FROM dbo.coviddeaths dea
@@ -149,3 +150,41 @@ WHERE dea.continent is not null
 
 SELECT * 
 FROM PercentPopVaccinated
+
+
+
+----FOr Visualizing Data in Tableau public, have to write data to excel as no SQL connector
+---- Simplified Queries for tableau
+
+--1. World Death Percentage
+SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths , (SUM(new_deaths)/SUM(new_cases))*100 AS DeathPercentage
+FROM dbo.coviddeaths
+WHERE continent is null and location like '%world%'
+
+--2. Total Death Count by Continent
+SELECT location, SUM(new_deaths) AS TotalDeathCount
+FROM dbo.coviddeaths
+WHERE continent is null
+and location not like '%income'
+and location not like '%Union'
+and location not like '%World'
+GROUP BY location
+ORDER BY TotalDeathCount desc
+
+--3. Highest infection rate by country at any time
+SELECT location, population, MAX(total_cases) as HighestInfectionCount, (MAX(total_cases)/population)*100 AS PecentPopulationInfected
+FROM dbo.coviddeaths
+WHERE continent is not null
+GROUP BY location,population
+ORDER BY PecentPopulationInfected desc
+
+
+--4.daily rate of infection by country
+SELECT location, population, date, MAX(total_cases) as HighestInfectionCount, (MAX(total_cases)/population)*100 AS PecentPopulationInfected
+FROM dbo.coviddeaths
+WHERE continent is not null
+GROUP BY location,population,date
+ORDER BY location,date
+
+
+
